4cf957dca3b1cc1723adb551e075f0a49e0b272d
carannantea <79670978+carannantea@users.noreply.github.com>
2021-10-08 19:55:26 -0400

    Added two layers for population and volcano markers

--- WebApp/mapping/map1.py
+++ WebApp/mapping/map1.py
@@ -23,7 +23,8 @@ def color_producer(elevation):
 map = folium.Map(location=[lats[0], lons[0]], zoom_start=6, tiles="Stamen Toner")
 
 # Create layer for all markers
-fg = folium.FeatureGroup(name="My Map")
+fg_pop = folium.FeatureGroup(name="Population")
+fg_vol = folium.FeatureGroup(name="Volcanoes")
 
 html = """<h4>Volcano information:</h4>
 Height: %s m <br/>
@@ -31,16 +32,19 @@ Location: %s
 """
 
 # Add country borders to the map as a layer 
-fg.add_child(folium.GeoJson(data=open('WebApp/mapping/world.json', 'r', encoding='utf=8=sig').read(), style_function=lambda x: {'fillColor':'blue' if x['properties']['POP2005'] < 10000000 else 'orange' if 10000000 <= x['properties']['POP2005'] < 20000000 else 'red'}))
+fg_pop.add_child(folium.GeoJson(data=open('WebApp/mapping/world.json', 'r', encoding='utf=8=sig').read(), style_function=lambda x: {'fillColor':'blue' if x['properties']['POP2005'] < 10000000 else 'orange' if 10000000 <= x['properties']['POP2005'] < 20000000 else 'red'}))
 
 # Iterate through lists to place new markers for each volcano
 for lt, ln, el, lc in zip(lats, lons, elev, locs):
     iframe = folium.IFrame(html=html % (str(el), lc), width=200, height=200)
     
-    fg.add_child(folium.CircleMarker(location=[lt,ln], popup=folium.Popup(iframe), radius=6, color='grey', fill_color=color_producer(el), fill_opacity=0.8))
+    fg_vol.add_child(folium.CircleMarker(location=[lt,ln], popup=folium.Popup(iframe), radius=6, color='grey', fill_color=color_producer(el), fill_opacity=0.8))
 
 # Add layers to the map
-map.add_child(fg)
+map.add_child(fg_pop)
+map.add_child(fg_vol)
+
+map.add_child(folium.LayerControl())
 
 # Save to HTML file
 map.save("Map1.html")
